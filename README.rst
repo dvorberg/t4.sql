2537ccd72ab37ceaa92f1919c774b80b05087a6e
Diedrich Vorberg <diedrich@tux4web.de>
2018-05-19 15:48:11 +0200

    Initial commit as t4.sql

---
+++ README.rst
@@ -0,0 +1,5 @@
+This module provides a simple wrapper for SQL within Python. The idea
+is to obscure the SQL code that is being generated as little as
+possible, but to hide all the gorry details, especially of quoting and
+escaping things, away from the programmer. Also this code is supposed
+to be backend independent. 
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = open("version.txt").read().strip()
+
+setup(name="t4.sql",
+      version=version,
+      description=("This module provides a simple "
+                   "wrapper for SQL within Python."),
+      long_description=open("README.rst").read(),
+      
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: SQL",
+      ],      
+      keywords="Database SQL",
+      
+      author="Diedrich Vorberg",
+      author_email="diedrich@tux4web.de",
+      url="https://github.com/dvorberg/t4.sql",
+      
+      license="GPL",
+      
+      packages=find_packages(exclude=["ez_setup"]),
+      namespace_packages=["t4"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "setuptools",
+          # -*- Extra requirements: -*-
+      ]
+)
